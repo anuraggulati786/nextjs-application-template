f62b888a06fa0ad83594e349b2ef5f1432a1cb4a
Anurag_786 <76244073+anuraggulati786@users.noreply.github.com>
2025-07-22 17:14:18 +0530

    Add .next/static/chunks/src_app_profile_page_tsx_a993527f._.js 1753184657702

---
+++ .next/static/chunks/src_app_profile_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_e86e494a._.js",
+  "static/chunks/node_modules_01034117._.js"
+],
+    source: "dynamic"
+});
